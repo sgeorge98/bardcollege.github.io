1fd5065a2407ef978b901a150e514845fb72f723
sgeorge98 <sgeorge98@users.noreply.github.com>
2016-08-11 12:09:08 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Literature", "Cinema", "Engineering", "Theater", "Math", "Architecture"];
-var programmers = ["writer", "artist", "choreographer", "director", "mathematician", "architect"]
+var programmers = ["writer", "actor", "choreographer", "director", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
